ee78c1585b5f137b56d8981d7f330e35310b8fa5
Cyprien Aoustin <cyprien.aoustin@icloud.com>
2026-05-04 14:51:50 +0200

    Start StockResource Add Get by ProduitId Add POST

--- service-stock/src/main/java/fr/formation/api/StockResource.java
+++ service-stock/src/main/java/fr/formation/api/StockResource.java
@@ -1,14 +1,71 @@
 package fr.formation.api;
 
+import java.util.Map;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import fr.formation.api.dto.request.CreateOrUpdateStockRequest;
+import fr.formation.clientRest.ProduitClientRest;
+import fr.formation.model.Stock;
 import fr.formation.repo.StockRepository;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
 
 @Path("/api/stock")
 public class StockResource {
-    private final StockRepository repository;
+
+    @RestClient
+    @Inject
+    private ProduitClientRest produitClientRest;
+
+    @Inject
+    private StockRepository repository;
 
     public StockResource(StockRepository repository) {
         this.repository = repository;
     }
+
+    @GET
+    @Path("/{produitId}")
+    public Stock findStockByProduitId(@PathParam("produitId") Integer produitId) {
+        return this.repository.findById(produitId);
+    }
+
+    @Transactional
+    @POST
+    public Response create(@Valid CreateOrUpdateStockRequest request) {
+
+        try {
+            boolean findProduitById = this.produitClientRest.findProduitById(request.produitId());
+
+            if (!findProduitById) {
+                return Response.status(Response.Status.FORBIDDEN)
+                        .entity(Map.of("findProduit", false))
+                        .build();
+            }
+        }
+
+        catch (Exception e) {}
+
+        Stock stock = new Stock();
+
+        stock.setQuantite(request.quantite());
+        stock.setProduitId(request.produitId());
+
+        this.repository.persist(stock);
+
+        return Response.status(Response.Status.CREATED)
+                .entity(stock.getId())
+                .build();
+
+    }
+
+
 
 }
